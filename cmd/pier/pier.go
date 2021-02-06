9cf1796dbc8b9bd1c52345c661fcbe140be2f644
Levi Harrison <levisamuelharrison@gmail.com>
2021-02-05 23:12:33 -0500

    Begun fsnotify utilization

--- cmd/pier/pier.go
+++ cmd/pier/pier.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/leviharrison/pier/parse"
 )
 
 func main() {
-	for _, i := range parse.Files("cmd/pier") {
-		fmt.Println(i)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Printf("Could not create watcher: %v\n", err)
+		os.Exit(1)
+	}
+	defer watcher.Close()
+
+	files := parse.Files("cmd/pier")
+}
+
+func watch(watcher fsnotify.Watcher) {
+	for {
+		select {}
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/leviharrison/pier
 
 go 1.15
 
-require golang.org/x/mod v0.4.1
+require (
+	github.com/fsnotify/fsnotify v1.4.9
+	golang.org/x/mod v0.4.1
+)
