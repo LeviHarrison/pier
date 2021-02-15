ef61456d6e637ccb7cd38a0ca72137b762da40c8
Levi Harrison <levisamuelharrison@gmail.com>
2021-02-14 21:23:25 -0500

    Worked on implementing docker client

--- cmd/pier/pier.go
+++ cmd/pier/pier.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/docker/docker/client"
 	"github.com/leviharrison/pier/parse"
 	"github.com/leviharrison/pier/watch"
 )
@@ -14,6 +16,14 @@ Usage: pier /directory/of/your/main/package your.Dockerfile
 `
 
 func main() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		fmt.Printf("Error initializing docker client: %v\n", err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
 	args := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println(help)
